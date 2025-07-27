06281ba6c25d80145eee87ad7e2f3e324f3a53f6
BasharatAli1 <basharatali0567@gmail.com>
2025-07-27 14:02:37 +0500

    convert a string to reverse order of characters

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,38 @@
-function print(): void {
-    console.log('>>> TS');
+function split(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    descriptor.value = function(...args: any []) {
+        const [arg] = args;
+        const splittedArg = arg.split('');
+        originalMethod.apply(this, [splittedArg]);
+    }
 }
 
-print();
+function reverse(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    descriptor.value = function(...args: any []) {
+        const [arg] = args;
+        const reversedArg = arg.reverse();
+        originalMethod.apply(this, [reversedArg]);
+    }
+}
+
+function join(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    descriptor.value = function(...args: any []) {
+        const [arg] = args;
+        const joinedArg = arg.join('');
+        originalMethod.apply(this, [joinedArg]);
+    }
+}
+
+class StringManager {
+    @split
+    @reverse
+    @join
+    print(str: string): void {
+        console.log(str);
+    }
+}
+
+const stringManager = new StringManager();
+stringManager.print('hello');
